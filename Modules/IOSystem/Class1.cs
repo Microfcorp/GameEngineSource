ffa1943c54ba4e12b104815ed33dde422b5f6a42
Alesha <a.mostowenko2012@yandex.ru>
2018-09-22 17:35:36 +0300

    Add files via upload

---
+++ Modules/IOSystem/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SystemModule;
+using System.IO;
+
+namespace GameModule
+{
+    public class GameModule
+    {
+        public void SetParam(CommandData data)
+        {
+            if(data.Name == "ReadFile")
+            {
+                ReadFile(data.Params["path"]);
+            }
+            else if (data.Name == "WriteFile")
+            {
+                WriteFile(data.Params["path"], data.Params["text"]);
+            }
+            else if (data.Name == "ReadAndWrire")
+            {
+                ReadAndWrite(data.Params["path"], data.Params["text"], data.Params["split"]);
+            }
+        }
+
+        public void ReadFile(string path)
+        {
+            Console.WriteLine(File.ReadAllText(path));
+        }
+        public void WriteFile(string path, string text)
+        {
+            File.WriteAllText(path, text);
+        }
+        public void ReadAndWrite(string path, string text, string split)
+        {
+            string str = File.ReadAllText(path);
+            File.WriteAllText(path, str + split + text);
+        }
+    }
+}
